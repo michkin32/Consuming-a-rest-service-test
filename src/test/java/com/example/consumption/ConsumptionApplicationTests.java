e350b1d90783487bd6c3ac30946bbc33804a787f
Mike King <michkin32@gmail.com>
2019-12-29 20:03:03 -0500

    Initialization

---
+++ src/test/java/com/example/consumption/ConsumptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
